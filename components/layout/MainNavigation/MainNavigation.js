2dd48cb6358bb954e76aa261711e336d5b83b1ab
stephendewyer <stephen.dewyer@stephengdewyer.info>
2022-07-20 22:32:52 -0500

    changing navbar styles

--- components/layout/MainNavigation/MainNavigation.js
+++ components/layout/MainNavigation/MainNavigation.js
@@ -36,7 +36,7 @@ const MainNavigation = (props) => {
     const navbarStyles = {
         position: 'fixed',
         width: '100%',
-        height: '250px',
+        height: 'auto',
         top: '0',
         left: '0',
         right: '0',
