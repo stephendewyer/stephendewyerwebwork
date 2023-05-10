b2e1174d218eb0f59f24c80c54e69243c22440db
stephendewyer <stephen.dewyer@stephengdewyer.info>
2023-05-10 15:59:02 -0500

    updating case studies page

--- pages/case_studies/index.js
+++ pages/case_studies/index.js
@@ -1,18 +1,19 @@
 import Head from 'next/head';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Button from '../../components/button/button.js';
-import classes from './case_studies.module.css';
+import styles from './case_studies.module.css';
 import Image from 'next/image';
-import MGTrailerScreengrab from '../../public/images/case_studies/MGTrailer/MGTrailer_index_top.JPG';
 import joyfoodlyScreengrab from '../../public/images/case_studies/Joyfoodly/Joyfoodly_desktop.png';
 import artinTechServicesScreengrab from '../../public/images/case_studies/Art_in_Tech_Services_Inc/Art_in_Tech_Services_screengrab.png';
 import publicArtsCommissionScreengrab from '../../public/images/case_studies/Public_Arts_Commission/Public_Arts_Commission_screengrab.png';
 import infiniteMileScreengrab from '../../public/images/case_studies/Infinite_Mile_LLC/Infinite_Mile_Screengrab.png';
-import Link from 'next/link';
 import MyLink from '../../public/util/myLink';
 
 const CaseStudiesPage = () => {
-
+    const [joyfoodlyIsHovered, setJoyfoodlyIsHovered] = useState(false);
+    const [artInTechServicesIsHovered, setArtInTechServicesIsHovered] = useState(false);
+    const [publicArtsCommissionIsHovered, setPublicArtsCommissionIsHovered] = useState(false);
+    const [infiniteMileIsHovered, setInfiniteMileIsHovered] = useState(false);
     return (
         <Fragment>
             <Head>
@@ -22,106 +23,138 @@ const CaseStudiesPage = () => {
                 <meta property="og:url" content="https://stephendewyerwebwork.vercel.app/case_studies" />
             </Head>
             <div className="container">
-                <div className={classes.case_studies}>
+                <div className={styles.case_studies}>
                     <h1 className="header">
                         case studies
                     </h1>
-                    <div className={classes.case_studies_a}>
-                        <div className={classes.case_study_image}>
-                            <MyLink href="/case_studies/joyfoodly" passHref >
-                                <Image src={joyfoodlyScreengrab} layout="responsive" alt="Joyfoodly screenshot" />
-                            </MyLink>
+                    <MyLink 
+                        onMouseEnter={(() => setJoyfoodlyIsHovered(true))}
+                        onMouseOver={(() => setJoyfoodlyIsHovered(true))}
+                        onMouseLeave={(() => setJoyfoodlyIsHovered(false))}
+                        onMouseOut={(() => setJoyfoodlyIsHovered(false))}
+                        href="/case_studies/joyfoodly" 
+                        passHref 
+                    >
+                        <div className={styles.case_studies_a}>
+                        <div className={styles.case_study_image}>  
+                            <Image src={joyfoodlyScreengrab} layout="responsive" alt="Joyfoodly screenshot" /> 
                         </div>
-                        <div className={classes.case_study_info_container}>
-                            <div className={classes.case_study_info}>
-                                <h3 className={classes.case_study_title}>
-                                    Joyfoodly legacy website<br /><span style={{color: "#545451"}} >2022</span>
-                                </h3>
-                                <div className={classes.case_study_subtitle}>
-                                    A legacy website for a culinary education company serving families.
-                                </div>
-                                <Link href="/case_studies/joyfoodly" passHref >
-                                    <div className={classes.view_button} >
-                                        <Button aria-label="link to view">
-                                            view
-                                        </Button>
-                                    </div>
-                                </Link>
+                        <div className={styles.case_study_info_container}>
+                            <div className={styles.case_study_info}>
+                            <h3 className={styles.case_study_title}>
+                                Joyfoodly legacy website<br /><span style={{color: "#52535E"}} >2022</span>
+                            </h3>
+                            <div className={styles.case_study_subtitle}>
+                                A legacy website for a culinary education company serving families.
+                            </div>
+                            <div className={styles.view_button} >
+                                <Button 
+                                buttonIsActive={joyfoodlyIsHovered}
+                                aria-label="link to view"
+                                >
+                                view
+                                </Button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </MyLink>
+                    <MyLink 
+                        onMouseEnter={(() => setArtInTechServicesIsHovered(true))}
+                        onMouseOver={(() => setArtInTechServicesIsHovered(true))}
+                        onMouseLeave={(() => setArtInTechServicesIsHovered(false))}
+                        onMouseOut={(() => setArtInTechServicesIsHovered(false))}
+                        href="/case_studies/artInTechServices" 
+                        passHref 
+                    >
+                        <div className={styles.case_studies_b}>
+                        <div className={styles.case_study_info_container}>
+                            <div className={styles.case_study_info}>
+                            <h3 className={styles.case_study_title}>
+                                Art in Tech Services Inc web programs<br/><span style={{color: "#52535E"}} >2021 &ndash; present</span>
+                            </h3>
+                            <div className={styles.case_study_subtitle}>
+                                Web programs for a company that uses advancements in art and digital technology to improve design for businesses and the public.
+                            </div>
+                            <div className={styles.view_button} >
+                                <Button 
+                                    buttonIsActive={artInTechServicesIsHovered}
+                                    aria-label="link to view"
+                                >
+                                    view
+                                </Button>
+                            </div>
+                            </div>
+                        </div>
+                        <div className={styles.case_study_image}>
+                            <Image src={artinTechServicesScreengrab} layout="responsive" alt="Art in Tech Services Inc screenshot" />
+                        </div>
+                        </div>
+                    </MyLink>
+                    <MyLink 
+                        onMouseEnter={(() => setPublicArtsCommissionIsHovered(true))}
+                        onMouseOver={(() => setPublicArtsCommissionIsHovered(true))}
+                        onMouseLeave={(() => setPublicArtsCommissionIsHovered(false))}
+                        onMouseOut={(() => setPublicArtsCommissionIsHovered(false))}
+                        href="/case_studies/publicArtsCommission" 
+                        passHref 
+                    >
+                        <div className={styles.case_studies_a}>
+                        <div className={styles.case_study_image}>
+                            <Image src={publicArtsCommissionScreengrab} layout="responsive" alt="Public Arts Commission screenshot" />              
+                        </div>
+                        <div className={styles.case_study_info_container}>
+                            <div className={styles.case_study_info}>
+                            <h3 className={styles.case_study_title}>
+                                Public Arts Commission web programs<br/><span style={{color: "#52535E"}} >2017 &ndash; present</span>
+                            </h3>
+                            <div className={styles.case_study_subtitle}>
+                                Web programs for a non-profit organization that connects the art community to democratic movements.
+                            </div>
+                            <div className={styles.view_button} >
+                                <Button 
+                                    buttonIsActive={publicArtsCommissionIsHovered}
+                                    aria-label="link to view"
+                                >
+                                    view
+                                </Button>
+                            </div>
                             </div>
                         </div>
                     </div>
-                    <div className={classes.case_studies_b}>
-                        <div className={classes.case_study_info_container}>
-                            <div className={classes.case_study_info}>
-                                <h3 className={classes.case_study_title}>
-                                    Art in Tech Services Inc web programs<br/><span style={{color: "#545451"}} >2021 &ndash; present</span>
-                                </h3>
-                                <div className={classes.case_study_subtitle}>
-                                    Web programs for a company that uses advancements in art and digital technology to improve design for businesses and the public.
-                                </div>
-                                <Link href="/case_studies/artInTechServices" passHref >
-                                    <div className={classes.view_button} >
-                                        <Button aria-label="link to view">
-                                            view
-                                        </Button>
-                                    </div>
-                                </Link>
+                    </MyLink>
+                    <MyLink 
+                        onMouseEnter={(() => setInfiniteMileIsHovered(true))}
+                        onMouseOver={(() => setInfiniteMileIsHovered(true))}
+                        onMouseLeave={(() => setInfiniteMileIsHovered(false))}
+                        onMouseOut={(() => setInfiniteMileIsHovered(false))}
+                        href="/case_studies/infiniteMile" 
+                        passHref 
+                    >
+                        <div className={styles.case_studies_b}>
+                        <div className={styles.case_study_info_container}>
+                            <div className={styles.case_study_info}>
+                            <h3 className={styles.case_study_title}>
+                            Infinite Mile LLC website<br/><span style={{color: "#52535E"}} >2013 &ndash; 2017</span>
+                            </h3>
+                            <div className={styles.case_study_subtitle}>
+                            A website for an award-winning journal of art and culture(s) in Detroit, Michigan.
+                            </div>
+                            <div className={styles.view_button} >
+                                <Button 
+                                    buttonIsActive={infiniteMileIsHovered}
+                                    aria-label="link to view"
+                                >
+                                    view
+                                </Button>
+                            </div>
                             </div>
                         </div>
-                        <div className={classes.case_study_image}>
-                            <MyLink href="/case_studies/artInTechServices" passHref >
-                                <Image src={artinTechServicesScreengrab} layout="responsive" alt="Art in Tech Services Inc screenshot" />
-                            </MyLink>
+                        <div className={styles.case_study_image}>
+                        <Image src={infiniteMileScreengrab} layout="responsive" alt="Infinite Mile screenshot" />
                         </div>
-                    </div>
-                    <div className={classes.case_studies_a}>
-                        <div className={classes.case_study_image}>
-                            <MyLink href="/case_studies/publicArtsCommission" passHref >
-                                <Image src={publicArtsCommissionScreengrab} layout="responsive" alt="Public Arts Commission screenshot" />
-                            </MyLink>                    
                         </div>
-                        <div className={classes.case_study_info_container}>
-                            <div className={classes.case_study_info}>
-                                <h3 className={classes.case_study_title}>
-                                    Public Arts Commission web programs<br/><span style={{color: "#545451"}} >2017 &ndash; present</span>
-                                </h3>
-                                <div className={classes.case_study_subtitle}>
-                                    Web programs for a non-profit organization that connects the art community to democratic movements.
-                                </div>
-                                <Link href="/case_studies/publicArtsCommission" passHref >
-                                    <div className={classes.view_button} >
-                                        <Button aria-label="link to view">
-                                            view
-                                        </Button>
-                                    </div>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={classes.case_studies_b}>
-                        <div className={classes.case_study_info_container}>
-                            <div className={classes.case_study_info}>
-                                <h3 className={classes.case_study_title}>
-                                    Infinite Mile LLC website<br/><span style={{color: "#545451"}} >2013 &ndash; 2017</span>
-                                </h3>
-                                <div className={classes.case_study_subtitle}>
-                                    A website for an award-winning journal of art and culture(s) in Detroit, Michigan.
-                                </div>
-                                <Link href="/case_studies/infiniteMile" passHref >
-                                    <div className={classes.view_button} >
-                                        <Button aria-label="link to view">
-                                            view
-                                        </Button>
-                                    </div>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className={classes.case_study_image}>
-                            <MyLink href="/case_studies/infiniteMile" passHref >
-                                <Image src={infiniteMileScreengrab} layout="responsive" alt="Infinite Mile screenshot" />
-                            </MyLink>
-                        </div>
-                    </div>
+                    </MyLink>
                 </div>
             </div>
         </Fragment>
