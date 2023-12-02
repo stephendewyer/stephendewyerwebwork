f0c3a90c4ab420eb2ceea53f3485f22936f7f5c4
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2023-12-01 19:39:26 -0600

    updating public arts commission carousel

--- components/slide_carousel/PublicArtsCommissionSlideCarousel.js
+++ components/slide_carousel/PublicArtsCommissionSlideCarousel.js
@@ -5,11 +5,20 @@ import orange_left_arrow from '../../public/images/arrows/arrow_left_01.png';
 import green_left_arrow from '../../public/images/arrows/arrow_left_02.png';
 import orange_right_arrow from '../../public/images/arrows/arrow_right_01.png';
 import green_right_arrow from '../../public/images/arrows/arrow_right_02.png';
-import slide_01 from '../../public/images/case_studies/Public_Arts_Commission/mockups/Public_Arts_Commission_case_study_01.jpg';
-import slide_02 from '../../public/images/case_studies/Public_Arts_Commission/mockups/Public_Arts_Commission_case_study_02.jpg';
-import slide_03 from '../../public/images/case_studies/Public_Arts_Commission/mockups/Public_Arts_Commission_case_study_03.jpg';
-import slide_04 from '../../public/images/case_studies/Public_Arts_Commission/mockups/Public_Arts_Commission_case_study_04.jpg';
-import slide_05 from '../../public/images/case_studies/Public_Arts_Commission/mockups/Public_Arts_Commission_case_study_05.jpg';
+import slide_01 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_01.jpg';
+import slide_02 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_02.jpg';
+import slide_03 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_03.jpg';
+import slide_04 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_04.jpg';
+import slide_05 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_05.jpg';
+import slide_06 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_06.jpg';
+import slide_07 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_07.jpg';
+import slide_08 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_08.jpg';
+import slide_09 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_09.jpg';
+import slide_10 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_10.jpg';
+import slide_11 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_11.jpg';
+import slide_12 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_12.jpg';
+import slide_13 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_13.jpg';
+import slide_14 from '../../public/images/case_studies/public_arts_commission_redesign/public_arts_commission_redesign_14.jpg';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -92,6 +101,33 @@ export default class PublicArtsCommissionSlideCarousel extends Component {
           </div>
           <div key={5}>
             <Image src={slide_05} alt="Public Arts Commission slide 5" priority/>
+          </div>
+          <div key={6}>
+            <Image src={slide_06} alt="Public Arts Commission slide 6" priority/>
+          </div>
+          <div key={7}>
+            <Image src={slide_07} alt="Public Arts Commission slide 7" priority/>
+          </div>
+          <div key={8}>
+            <Image src={slide_08} alt="Public Arts Commission slide 8" priority/>
+          </div>
+          <div key={9}>
+            <Image src={slide_09} alt="Public Arts Commission slide 9" priority/>
+          </div>
+          <div key={10}>
+            <Image src={slide_10} alt="Public Arts Commission slide 10" priority/>
+          </div>
+          <div key={11}>
+            <Image src={slide_11} alt="Public Arts Commission slide 11" priority/>
+          </div>
+          <div key={12}>
+            <Image src={slide_12} alt="Public Arts Commission slide 12" priority/>
+          </div>
+          <div key={13}>
+            <Image src={slide_13} alt="Public Arts Commission slide 13" priority/>
+          </div>
+          <div key={14}>
+            <Image src={slide_14} alt="Public Arts Commission slide 14" priority/>
           </div>
         </Slider>
         <div className={classes.arrows}>
