e6e59a63ab4f9b347670bd9cd8bb21b11a7cb826
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2024-01-28 20:52:29 -0600

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import Layout from '../components/layout/Layout';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return(
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
   ) 
 };
