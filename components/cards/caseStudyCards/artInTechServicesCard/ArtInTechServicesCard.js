b1c7af1609aa11b7541c11ed719a6d137f2df354
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2024-02-24 06:36:14 -0500

    updating case study cards

--- components/cards/caseStudyCards/artInTechServicesCard/ArtInTechServicesCard.js
+++ components/cards/caseStudyCards/artInTechServicesCard/ArtInTechServicesCard.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import ArtInTechServicesMobilePrototype from '../../../../public/images/case_studies/Art_in_Tech_Services_Inc/prototypes/Art_in_Tech_Services_Inc_web_prototype_07.png';
 import ArtInTechServicesDesktopPrototype from '../../../../public/images/case_studies/Art_in_Tech_Services_Inc/prototypes/Art_in_Tech_Services_Inc_web_prototype_laptop_02.png';
 import ArtInTechServicesBackground from '../../../../public/images/case_studies/Art_in_Tech_Services_Inc/background/Art_in_Tech_Services_background.jpg';
-import { useState, Fragment } from 'react';
+import { useState } from 'react';
 
 const ArtInTechServicesCard = () => {
 
@@ -13,82 +13,85 @@ const ArtInTechServicesCard = () => {
     const [videoIsHovered, setVideoIsHovered] = useState(false);
 
     return (
-        <MyLink 
-            onMouseEnter={(() => setArtInTechServicesIsHovered(true))}
-            onMouseOver={(() => setArtInTechServicesIsHovered(true))}
-            onMouseLeave={(() => setArtInTechServicesIsHovered(false))}
-            onMouseOut={(() => setArtInTechServicesIsHovered(false))}
-            href="/case_studies/artInTechServices" 
-            passHref 
-          >
-            <div className={artInTechServicesIsHovered? styles.case_studies_b_hovered : styles.case_studies_b}>
-              <div className={styles.case_study_info_container}>
-                <div className={styles.case_study_info}>
-                  <h3>
-                      Art in Tech Services Inc web platform
-                  </h3>
-                  <h4>
-                    2021 &ndash; present
-                  </h4>
-                  <p>
-                      A web platform for a company that combines the vision and technical expertise to make groundbreaking digital products.
-                  </p>
-                  <div className={styles.view_button} >
-                      <ButtonAction
-                        buttonIsActive={artInTechServicesIsHovered}
-                        aria-label="link to view"
-                      >
-                          view
-                      </ButtonAction>
-                  </div>
-                </div>
-              </div>
-              <div 
-                className={styles.case_study_image}
-                onMouseEnter={(() => setVideoIsHovered(true))}
-                onMouseOver={(() => setVideoIsHovered(true))}
-                onMouseLeave={(() => setVideoIsHovered(false))}
-                onMouseOut={(() => setVideoIsHovered(false))}
-              >
-                {videoIsHovered ? 
-                  <video autoPlay muted loop className={styles.video}>         
-                      <source src="/videos/Art_in_Tech_Services_interaction.mp4" type="video/mp4"/>       
-                  </video>
-                :
-                  <Fragment>
-                    <Image 
-                      src={ArtInTechServicesBackground} 
-                      layout="fill"
-                      objectFit="cover"
-                      alt="scene of Hal from 2001: A Space Odyssey directed by Stanley Kubrick" 
-                      priority
-                    />
-                    <div className={styles.case_study_overlay} />
-                    <div className={styles.case_study_prototypes}>
-                      <div className={styles.case_study_prototype_mobile}>
-                        <Image
-                          src={ArtInTechServicesMobilePrototype}
-                          alt="Art in Tech Services mobile prototype"
-                          layout="fill"
-                          objectFit="contain"
-                          priority
-                        />
-                      </div>
-                      <div className={styles.case_study_prototype_desktop}>
-                        <Image
-                          src={ArtInTechServicesDesktopPrototype}
-                          alt="Art in Tech Services desktop prototype"
-                          layout="fill"
-                          objectFit="contain"
-                          priority
-                        />
-                      </div>
-                    </div>
-                  </Fragment>
-                }
+      <MyLink 
+        onMouseEnter={(() => setArtInTechServicesIsHovered(true))}
+        onMouseOver={(() => setArtInTechServicesIsHovered(true))}
+        onMouseLeave={(() => setArtInTechServicesIsHovered(false))}
+        onMouseOut={(() => setArtInTechServicesIsHovered(false))}
+        href="/case_studies/artInTechServices" 
+        passHref 
+      >
+        <div className={artInTechServicesIsHovered? styles.case_studies_b_hovered : styles.case_studies_b}>
+          <div className={styles.case_study_info_container}>
+            <div className={styles.case_study_info}>
+              <h3>
+                  Art in Tech Services Inc web platform
+              </h3>
+              <h4>
+                2021 &ndash; present
+              </h4>
+              <p>
+                  A web platform for a company that combines the vision and technical expertise to make groundbreaking digital products.
+              </p>
+              <div className={styles.view_button} >
+                  <ButtonAction
+                    buttonIsActive={artInTechServicesIsHovered}
+                    aria-label="link to view"
+                  >
+                      view
+                  </ButtonAction>
               </div>
             </div>
-          </MyLink>
+          </div>
+          <div 
+            className={styles.case_study_image}
+            onMouseEnter={(() => setVideoIsHovered(true))}
+            onMouseOver={(() => setVideoIsHovered(true))}
+            onMouseLeave={(() => setVideoIsHovered(false))}
+            onMouseOut={(() => setVideoIsHovered(false))}
+          >
+            <video 
+              autoPlay 
+              muted 
+              loop 
+              className={styles.video} 
+              style={{display: videoIsHovered ? "block" : "none" }}
+            >         
+              <source src="/videos/Art_in_Tech_Services_interaction.mp4" type="video/mp4"/>       
+            </video>
+            <div style={{display: videoIsHovered ? "none" : "block" }}>
+              <Image 
+                src={ArtInTechServicesBackground} 
+                layout="fill"
+                objectFit="cover"
+                alt="scene of Hal from 2001: A Space Odyssey directed by Stanley Kubrick" 
+                priority
+              />
+              <div className={styles.case_study_overlay} />
+              <div className={styles.case_study_prototypes}>
+                <div className={styles.case_study_prototype_mobile}>
+                  <Image
+                    src={ArtInTechServicesMobilePrototype}
+                    alt="Art in Tech Services mobile prototype"
+                    layout="fill"
+                    objectFit="contain"
+                    priority
+                  />
+                </div>
+                <div className={styles.case_study_prototype_desktop}>
+                  <Image
+                    src={ArtInTechServicesDesktopPrototype}
+                    alt="Art in Tech Services desktop prototype"
+                    layout="fill"
+                    objectFit="contain"
+                    priority
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </MyLink>
     );
 }
 
--- components/cards/caseStudyCards/infiniteMileCard/InfiniteMileCard.js
+++ components/cards/caseStudyCards/infiniteMileCard/InfiniteMileCard.js
@@ -1,7 +1,7 @@
 import styles from './InfiniteMileCard.module.css';
 import MyLink from '../../../../public/util/myLink';
 import ButtonAction from '../../../buttons/buttonAction/ButtonAction';
-import { useState, Fragment } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import InfiniteMileBackground from '../../../../public/images/case_studies/Infinite_Mile_LLC/background/Infinite_Mile_background.jpg';
 import InfiniteMileDetroitMobilePrototype from '../../../../public/images/case_studies/Infinite_Mile_LLC/prototypes/infinite_mile_prototype_10.png';
@@ -12,73 +12,79 @@ const InfiniteMileCard = () => {
     const [videoIsHovered, setVideoIsHovered] = useState(false);
 
     return (
-        <MyLink 
-            onMouseEnter={(() => setInfiniteMileIsHovered(true))}
-            onMouseOver={(() => setInfiniteMileIsHovered(true))}
-            onMouseLeave={(() => setInfiniteMileIsHovered(false))}
-            onMouseOut={(() => setInfiniteMileIsHovered(false))}
-            href="/case_studies/infiniteMile" 
-            passHref 
-          >
-            <div className={infiniteMileIsHovered ? styles.case_studies_b_hovered : styles.case_studies_b}>
-              <div className={styles.case_study_info_container}>
-                <div className={styles.case_study_info}>
-                  <h3>
-                    Infinite Mile LLC website
-                  </h3>
-                  <h4>
-                    2013 &ndash; 2017
-                  </h4>
-                  <p>
-                    A website for an award-winning journal of art and culture(s) in Detroit, Michigan.
-                  </p>
-                  <div className={styles.view_button} >
-                      <ButtonAction 
-                        buttonIsActive={infiniteMileIsHovered}
-                        aria-label="link to view"
-                      >
-                          view
-                      </ButtonAction>
-                  </div>
+      <MyLink 
+          onMouseEnter={(() => setInfiniteMileIsHovered(true))}
+          onMouseOver={(() => setInfiniteMileIsHovered(true))}
+          onMouseLeave={(() => setInfiniteMileIsHovered(false))}
+          onMouseOut={(() => setInfiniteMileIsHovered(false))}
+          href="/case_studies/infiniteMile" 
+          passHref 
+        >
+          <div className={infiniteMileIsHovered ? styles.case_studies_b_hovered : styles.case_studies_b}>
+            <div className={styles.case_study_info_container}>
+              <div className={styles.case_study_info}>
+                <h3>
+                  Infinite Mile LLC website
+                </h3>
+                <h4>
+                  2013 &ndash; 2017
+                </h4>
+                <p>
+                  A website for an award-winning journal of art and culture(s) in Detroit, Michigan.
+                </p>
+                <div className={styles.view_button} >
+                    <ButtonAction 
+                      buttonIsActive={infiniteMileIsHovered}
+                      aria-label="link to view"
+                    >
+                        view
+                    </ButtonAction>
                 </div>
               </div>
-              <div 
-                onMouseEnter={(() => setVideoIsHovered(true))}
-                onMouseOver={(() => setVideoIsHovered(true))}
-                onMouseLeave={(() => setVideoIsHovered(false))}
-                onMouseOut={(() => setVideoIsHovered(false))}
-                className={styles.case_study_image}
-              >
-                {videoIsHovered ?
-                  <video autoPlay muted loop className={styles.video}>         
-                    <source src="/videos/Infinite_Mile_interactions.mp4" type="video/mp4"/>       
-                  </video>
-                :
-                <Fragment>
-                  <Image 
-                    src={InfiniteMileBackground} 
-                    alt="Detroit, Michigan as seen from Fisher Building in the morning" 
-                    layout="fill"
-                    objectFit="cover"
-                    priority
-                  />
-                  <div className={styles.case_study_overlay} />
-                  <div className={styles.case_study_prototypes}>
-                      <div className={styles.case_study_prototype_desktop}>
-                        <Image
-                          src={InfiniteMileDetroitMobilePrototype}
-                          alt="Infinite Mile desktop prototype"
-                          layout="fill"
-                          objectFit="contain"
-                          priority
-                        />
-                      </div>
-                    </div>
-                  </Fragment>
-                }
-              </div>
             </div>
-        </MyLink>
+            <div 
+              onMouseEnter={(() => setVideoIsHovered(true))}
+              onMouseOver={(() => setVideoIsHovered(true))}
+              onMouseLeave={(() => setVideoIsHovered(false))}
+              onMouseOut={(() => setVideoIsHovered(false))}
+              className={styles.case_study_image}
+            >
+              <video 
+                autoPlay 
+                muted 
+                loop 
+                className={styles.video}
+                style={{display: videoIsHovered ? "block" : "none" }}
+              >         
+                <source 
+                  src="/videos/Infinite_Mile_interactions.mp4" 
+                  type="video/mp4"
+                />       
+              </video>
+              <div style={{display: videoIsHovered ? "none" : "block" }}>
+                <Image 
+                  src={InfiniteMileBackground} 
+                  alt="Detroit, Michigan as seen from Fisher Building in the morning" 
+                  layout="fill"
+                  objectFit="cover"
+                  priority
+                />
+                <div className={styles.case_study_overlay} />
+                <div className={styles.case_study_prototypes}>
+                    <div className={styles.case_study_prototype_desktop}>
+                      <Image
+                        src={InfiniteMileDetroitMobilePrototype}
+                        alt="Infinite Mile desktop prototype"
+                        layout="fill"
+                        objectFit="contain"
+                        priority
+                      />
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+      </MyLink>
     );
 }
 
