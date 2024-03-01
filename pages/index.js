847dcb2e6924ffe8359ba7ce5df1a8a7f6647de3
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2024-03-01 16:22:34 -0500

    updating index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React, { Fragment, useEffect, useState, useRef } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import VideoJS from '../components/videoJS/videoJS'; // point to where the functional component is stored
 import styles from '../styles/Index.module.css';
 import MyLink from '../public/util/myLink';
@@ -11,6 +11,22 @@ import JoyfoodlyCard from '../components/cards/caseStudyCards/joyfoodlyCard/Joyf
 import InfiniteMileCard from '../components/cards/caseStudyCards/infiniteMileCard/InfiniteMileCard';
 
 const IndexPage = () => {
+
+  const [pageLoaded, setPageLoaded] = useState(false);
+
+  const [showIntroParagraph, setShowIntroParagraph] = useState(false);
+
+  useEffect(() => {
+    setPageLoaded(true);
+
+    const timeoutIntroParagraph = setTimeout(() => {
+      setShowIntroParagraph(true);
+    }, 750);
+
+    // cleanup function to clear the timeout if component unmounts
+    return () => clearTimeout(timeoutIntroParagraph);
+
+  }, []);
 
   const playerRef = React.useRef(null);
 
@@ -66,49 +82,24 @@ const IndexPage = () => {
 
   };
 
-  const welcomeHeading = useRef();
-  
-  const [welcomeIsVisible, setWelcomeIsVisible] = useState(false);
-
-  useEffect(() => {
-
-    const observer = new IntersectionObserver((entries) => {
-
-      const entry = entries[0];
-
-      setWelcomeIsVisible(entry.isIntersecting);
-
-    });
-
-    observer.observe(welcomeHeading.current);
-
-  }, []);
-
   return (
     <Fragment>
       <Head>
-        <title>stephen dewyer UX design and full-stack web development</title>
-        <meta name="description" content="portfolio of user experience (UX) design and full-stack web development by stephen garrett dewyer" />
+        <title>stephen dewyer UX design and software development</title>
+        <meta name="description" content="portfolio of user experience (UX) design and software development by stephen garrett dewyer" />
         <meta property="og:image" content="https://stephendewyerwebwork.vercel.app/videos/imagine_echo/imagine_echo_still_02.jpg" />
         <meta property="og:url" content="https://stephendewyerwebwork.vercel.app" />
       </Head>
       <div className="container">
-        <h1>
-          user experience (UX) design and full-stack web development
-        </h1>
+          <div className={styles.intro_paragraph}>
+            <p className={styles.paragraph}>
+              <span className={styles.welcome} style={pageLoaded ? {opacity: "100%"} : {opacity: "0"}}>welcome! </span>
+              <span className={styles.delayed_paragraph} style={showIntroParagraph ? {opacity: "100%"} : {opacity: "0"}}>My name is stephen dewyer.  I create ground-breaking software that helps empower communities to solve some of our most challenging problems by using my expertise in <span style={{fontWeight: "bold"}}>full-stack web development</span>, <span style={{fontWeight: "bold"}}>user experience (UX) design</span>, <span style={{fontWeight: "bold"}}>machine learning</span>, <span style={{fontWeight: "bold"}}>art</span> and as a <span style={{fontWeight: "bold"}}>founder</span></span>.
+            </p>
+          </div>
         <div className={styles.intro_video}>
           <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
         </div>
-        <h2 
-          ref={welcomeHeading}
-          className={styles.animatedHeading}
-          style={(welcomeIsVisible) ? {opacity: "100%"} : {opacity: "0"}}
-        >
-          welcome!
-        </h2>
-        <p className={styles.workSection}>
-            My name is <span style={{fontWeight: "bold"}}>stephen dewyer</span> and I&#39;m an award-winning <span style={{fontWeight: "bold"}}>UX designer</span>, <span style={{fontWeight: "bold"}}>full-stack web developer</span>, <span style={{fontWeight: "bold"}}>artist</span> and <span style={{fontWeight: "bold"}}>organizer</span> with <span style={{fontWeight: "bold"}}>over ten years of experience</span> creating web programs for <span style={{fontWeight: "bold"}}>public institutions</span>, <span style={{fontWeight: "bold"}}>businesses</span>, <span style={{fontWeight: "bold"}}>campaigns</span> and <span style={{fontWeight: "bold"}}>movements</span>.
-        </p>
         <h2>
           featured case studies
         </h2>
@@ -123,8 +114,7 @@ const IndexPage = () => {
         </h2>
         <div className={styles.aboutSection}>
           <p className={styles.myStory}>
-          I began my career in art and transitioned into software development.  
-          Creating groundbreaking web programs that solve problems for people using advancements in digital technology, art and design is my passion. 
+          I began my career in art and transitioned into software development.  With over a decade of experience using advancements in digital technology, art and design in my work, I’ve won numerous awards and worked with a variety of for-profit and non-profit organizations.
           </p>
           <div className={styles.learnAboutMeButton}>
             <MyLink href="/about" passHref={true}>
